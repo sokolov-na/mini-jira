631a43b25f39d57ae07fbb39d15dbcc1d989113f
sokolov-na <225724418+sokolov-na@users.noreply.github.com>
2026-09-22 00:01:43 +0300

    feat: add database and migrations

---
+++ src/mini_jira/database/models.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
